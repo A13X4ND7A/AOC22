7ed0893924f8a340aa20c9eecc846f215d828a49
A13X4ND7A <alex@alexfaller.uk>
2022-12-05 10:56:35 +0000

    day2 part1 complete

--- day_02/index.js
+++ day_02/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 const lines = fs
-.readFileSync('day_02/day_02_input_test.txt', {encoding: "utf-8"})
+.readFileSync('day_02/day_02_input.txt', {encoding: "utf-8"})
 .split(/\r?\n/)
 // console.log({lines})
 
@@ -25,6 +25,8 @@ const lines = fs
 // 3 draw
 // 6 win
 
+
+
 const shapeSelected = {
     A: 1,
     B: 2,
@@ -40,55 +42,62 @@ const outcome = {
     loose: 0
 }
 
-const toWin = {
-    X: 'C',
-    Y: 'A',
-    Z: 'B'
-
+const toLoose = {
+    A: 'B',
+    B: 'C',
+    C: 'A'
+}
+const playerInput = {
+    X: 'A',
+    Y: 'B',
+    Z: 'C'
 }
 
-const totalScore = 0
 
 
 const opponent = lines.map(line => {
-   letter = line.charAt(0)
-   score = shapeSelected[letter]
-   return {
-    letter: letter,
-    score: score
-   }
-  
+    letter = line?.charAt(0)
+    score = shapeSelected[letter]
+    return {
+        letter: letter,
+        score: score
+    }
+    
 })
 const player = lines.map(line => {
-   letter = line.charAt(2)
-   score = shapeSelected[letter]
-      return {
-    letter: letter,
-    score: score
-   }
-  
+    letter = line?.charAt(2)
+    score = shapeSelected[letter]
+    return {
+        letter: playerInput[letter],
+        score: score
+    }
+    
 })
+let totalScore = []
 
-
-// need to figure out who won. 
-// the the opponent needs to have the corrosponding value in to toWin in order for player to win
-// if player letter and opponent match then its a draw
-
-
-
-
-const playerScore = (play, opp) => {
-    player.map((player) => {
-       
-        if (player.letter === toWin[player.letter]) {
-            console.log('draw')
-        } else {
-            console.log('something else')
-        }
-    }) 
+const isAWinner = (item, index) => {
+    if(toLoose[item?.letter] === player[index].letter ) {
+        totalScore.push(player[index].score + 6)
+        // console.log('player won')
+    } else {
+    if (player[index].letter === opponent[index].letter){
+        totalScore.push(player[index].score + 3)
+       // console.log("draw")
+    }else {
+        totalScore.push(player[index].score)
+        // console.log('Player Lost')
+    }
+    }
 }
 
-console.log(playerScore(player, opponent))
+const opponentPlayed = (opp) => {
+    opp.forEach(isAWinner)
+    const total = totalScore?.reduce((acc, total) => {
+    return acc + total
+    }, 0)
+    console.log(total)
+}
+console.log(opponentPlayed(opponent))
 
 
 
