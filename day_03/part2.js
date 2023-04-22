5d5b8f77051f38fd2374b7fd46e501948c0eaf2c
Alexandra Faller <alex@alexfaller.uk>
2023-04-22 09:02:38 +0000

    Complete day 03 part 02

--- day_03/part2.js
+++ day_03/part2.js
@@ -2,17 +2,20 @@ const fs = require("fs");
 const { deflateRaw } = require("zlib");
 
 const lines = fs
-  .readFileSync("day_03/day_03_input_test.txt", { encoding: "utf-8" })
+  .readFileSync("day_03/day_03_input.txt", { encoding: "utf-8" })
   .split(/\r?\n/);
   
 /*
-For safety, the Elves are divided into groups of three. Every Elf carries a badge that identifies their group. For efficiency, within each group of three Elves, the badge is the only item type carried by all three Elves. That is, if a group's badge is item type B, then all three Elves will have item type B somewhere in their rucksack, and at most two of the Elves will be carrying any other item type.
 
-The problem is that someone forgot to put this year's updated authenticity sticker on the badges. All of the badges need to be pulled out of the rucksacks so the new authenticity stickers can be attached.
+- Groups of three elves
+- Elves carry badge that Identify their group which is the item shared by all three.
+- Every three lines is equal to a group of Elves
 
-Additionally, nobody wrote down which item type corresponds to each group's badges. The only way to tell which item type is the right one is by finding the one item type that is common between all three Elves in each group
+- find the common letter in every three lines
+- assign a letter priority to the common letter
+- sum up all priorities
+
 */
-
 const getLetterPriority = (letter) => {
   if (letter.toUpperCase() === letter) {
     return letter.charCodeAt(0) - 38; 
@@ -20,16 +23,22 @@ const getLetterPriority = (letter) => {
     return letter.charCodeAt(0) - 96;
   }
 };
+const groupsOfThree = lines.reduce((acc, line, index) => {
+  const groupIndex = Math.floor(index / 3);
+  if (!acc[groupIndex]) {
+    acc[groupIndex] = [];
+  }
+  acc[groupIndex].push(line);
+  return acc;
+}, []);
 
-const commonItems = lines.map((line) => {
-  console.log(line, "line");
-  const firstHalf = line.slice(0, line.length / 2); // get the first half of the line
-  const secondHalf = line.slice(line.length / 2); // get the second half of the line
-
-  return Array.from(
-    new Set([...firstHalf].filter((item) => secondHalf.includes(item)))
-  );
+const commonItems = groupsOfThree.map((group) => {
+  const letterSets = group.map((line) => new Set(line));
+  const intersection = [...letterSets.reduce((acc, set) => new Set([...acc].filter((letter) => set.has(letter))))];
+  return intersection;
 });
+
+console.log(commonItems)
 
 const priorities = new Map();
 
@@ -43,3 +52,5 @@ const totalPriority = Array.from(priorities.values()).reduce(
   (total, priority) => total + priority
 );
 console.log(totalPriority); 
+
+
